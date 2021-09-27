1f933cfcc6f7fe4a5d16420d329cf0f3d7f2734a
IsYet1 <Don@IsYet.com>
2021-09-27 18:53:14 -0500

    Through 2/3 of chapter 3

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -7,6 +7,8 @@ struct ContentView: View {
             HStack {
                 VStack(alignment: .leading) {
                     Text("Nightly tasks")
+                        .font(.title3)
+                        
                     Text("Check all windows")
                     Text("Check all doors")
                     Text("Check the safe is locked")
@@ -15,9 +17,12 @@ struct ContentView: View {
                     Text("Clear ice from sidewalks")
                     Text("Document \"strange and unusual\" occurences")
                     Text("Weekly Tasks")
+                        .font(.title3)
+                        .padding(.top)
                     Text("Monthly Tasks")
+                        .padding(.top)
                 }
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .padding(.all)
                 Spacer()
             }
             Spacer()
