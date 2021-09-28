630eb949fb692d15e95583685610dcb4b7b478d6
IsYet1 <Don@IsYet.com>
2021-09-28 17:36:31 -0500

    Done through section 3. Basic list

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -6,21 +6,52 @@ struct ContentView: View {
         VStack {
             HStack {
                 VStack(alignment: .leading) {
-                    Text("Nightly tasks")
-                        .font(.title3)
-                        
-                    Text("Check all windows")
-                    Text("Check all doors")
-                    Text("Check the safe is locked")
-                    Text("Check the mailbox")
-                    Text("Inspect security cameras")
-                    Text("Clear ice from sidewalks")
-                    Text("Document \"strange and unusual\" occurences")
-                    Text("Weekly Tasks")
-                        .font(.title3)
+                    // MARK: Night tasks
+                    Group {
+                        HStack {
+                            Text(Image(systemName: "moon.stars"))
+                                .foregroundColor(.yellow)
+                            Text("- Nightly tasks")
+                                .font(.title3)
+                                .foregroundColor(.yellow)
+                                .underline()
+                        }
+                            
+                        Text("- Check all windows")
+                        Text("- Check all doors")
+                        Text("- Check the safe is locked")
+                        Text("- Check the mailbox")
+                        Text("- Inspect security cameras")
+                        Text("- Clear ice from sidewalks")
+                        Text("- Document \"strange and unusual\" occurences")
+                        Text("- One item too many")
+                    }
+
+                    Divider()
+                    HStack {
+                        Text(Image(systemName: "sunset"))
+                            .foregroundColor(.yellow)
+                        Text("- Weekly Tasks")
+                            .font(.title3)
+                            .foregroundColor(.yellow)
+                            .underline()
                         .padding(.top)
-                    Text("Monthly Tasks")
-                        .padding(.top)
+                    }
+                    Text("- Check inside all vacant rooms")
+                    Text("- Walk the perimeter of the property")
+
+                    Divider()
+                    HStack {
+                        Text(Image(systemName: "calendar"))
+                            .foregroundColor(.yellow)
+                        Text("- Monthly Tasks")
+                            .underline()
+                            .foregroundColor(.yellow)
+                            .padding(.top)
+                    }
+                    Text("- Test Security alarm")
+                    Text("- Test motion detectors")
+                    Text("- Test smoke alarm")
                 }
                 .padding(.all)
                 Spacer()
