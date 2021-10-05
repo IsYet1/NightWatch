f228c697f920ad1ff6ff44db43795b649af3212f
IsYet1 <Don@IsYet.com>
2021-10-05 18:08:06 -0500

    Add binding and get parent-child working

--- NightWatch/Samples/TaskData0.swift
+++ NightWatch/Samples/TaskData0.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+struct TaskData0_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskData0()
+    }
+}
+
 struct TaskData0: View {
     @State var theTask = Task(name: "Check all windows", isComplete: false, lastCompleted: nil)
     var body: some View {
@@ -15,6 +21,16 @@ struct TaskData0: View {
                 Image(systemName: theTask.isComplete ? "checkmark.square" : "square")
                 Text(theTask.name)
             }
+            ControlPanel(theTask: self.$theTask)
+        }
+    }
+}
+
+
+struct ControlPanel: View {
+    @Binding var theTask: Task
+    var body: some View {
+        HStack{
             Button(action: {
                 theTask.isComplete = true
             }) {
@@ -26,11 +42,5 @@ struct TaskData0: View {
                 Text("Reset")
             }.padding(.top)
         }
-    }
-}
-
-struct TaskData0_Previews: PreviewProvider {
-    static var previews: some View {
-        TaskData0()
     }
 }
